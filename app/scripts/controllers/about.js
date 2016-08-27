1c3e0c1a64981a824ce6fdd88c9891e42e29f320
Sarah Ghidalia <Mileem@macbook-air-de-sarah.home>
2016-08-27 15:59:32 +0200

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cocktailAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cocktailAppApp
+ */
+angular.module('cocktailAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cocktailAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cocktailAppApp
+ */
+angular.module('cocktailAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'Test'
+    ];
+  });
