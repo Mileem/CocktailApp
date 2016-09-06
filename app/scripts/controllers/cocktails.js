ffe05279117f281d80ff43d2a90d789dedfddff4
Mileem <okarina71@yahoo.fr>
2016-09-06 19:03:56 +0200

    Update service with

--- app/scripts/controllers/cocktails.js
+++ app/scripts/controllers/cocktails.js
@@ -8,16 +8,26 @@
  * Controller of the cocktailAppApp
  */
 angular.module('cocktailAppApp')
-  .controller('CocktailsCtrl', function ($scope, $http, CocktailService) {
-    var queryURL = CocktailService.getCocktails();
+  .controller('CocktailsCtrl', function ($scope, CocktailService) {
 
-    $http.get(queryURL).success(function(data) {
+    CocktailService.getCocktails(function (data) {
       $scope.cocktails = data.results.bindings;
     });
+
+
+    $scope.getCocktails = function(event) {
+      var alcoolResource = event.target.attributes.resource.value;
+      CocktailService.getCokctailsWithFilter(alcoolResource, function (data) {
+        $scope.cocktails = data.results.bindings;
+      });
+    };
+
   })
   .controller('CocktailViewCtrl', function ($scope, $routeParams, $http, CocktailService) {
     var uri = 'http://dbpedia.org/resource/' + $routeParams.name;
     var queryURL = CocktailService.getCocktail(uri);
+
+    $scope.uri = uri;
 
     $http.get(queryURL).success(function(data) {
       $scope.cocktail = data.results.bindings[0];
--- app/scripts/services/cocktailservice.js
+++ app/scripts/services/cocktailservice.js
@@ -8,20 +8,23 @@
  * Factory in the cocktailAppApp.
  */
 angular.module('cocktailAppApp')
-  .factory('CocktailService', function (SparqlService) {
+  .factory('CocktailService', function ($http, SparqlService) {
     return {
       //List with uri and name of all cocktails of the world
-      getCocktails: function () {
+      getCocktails: function (callback) {
         var query = 'SELECT DISTINCT ?uri, ?name, ?image WHERE { ' +
           '?uri rdf:type ?type .' +
           '?uri ?prop_label ?name .' +
           'OPTIONAL { ?uri dbo:thumbnail ?image. }' +
           'FILTER ((?type IN (yago:Cocktail107911677, yago:MixedDrink107911371)) && (?prop_label IN (dbp:name, rdfs:label)) && (lang(?name) = \'en\'))' +
-         '}';
-        return SparqlService.getQueryURL(query);
+         '} ORDER BY ?name';
+         $http.get(SparqlService.getQueryURL(query)).
+         success(function(data, status) {
+           callback(data, status);
+         });
       },
       // Informations about one cocktail
-      getCocktail: function(uri) {
+      getCocktail: function(uri, callback) {
         var query = 'SELECT DISTINCT ?name, ?abstract, ?ingredients, ?image, ?preparation, ?garnish WHERE { ' +
           '<'+ uri +'> ?prop_label ?name ;' +
             'dbo:abstract ?abstract;' +
@@ -31,7 +34,22 @@ angular.module('cocktailAppApp')
           'OPTIONAL { <'+ uri +'> dbp:garnish ?garnish . }' +
           'FILTER (?prop_label IN (dbp:name, rdfs:label) && lang(?abstract) = \'en\')'+
          '}';
-         return SparqlService.getQueryURL(query);
+         $http.get(SparqlService.getQueryURL(query)).
+         success(function(data, status) {
+           callback(data, status);
+         });
+      },
+      getCokctailsWithFilter: function(filter, callback) {
+        var query = 'SELECT DISTINCT ?uri, ?name, ?image WHERE { ' +
+          '?uri rdf:type <'+ filter +'> .' +
+          '?uri ?prop_label ?name .' +
+          'OPTIONAL { ?uri dbo:thumbnail ?image. }' +
+          'FILTER ((?prop_label IN (dbp:name, rdfs:label)) && (lang(?name) = \'en\'))' +
+         '} ORDER BY ?name';
+         $http.get(SparqlService.getQueryURL(query)).
+         success(function(data, status) {
+           callback(data, status);
+         });
       }
     };
   });
